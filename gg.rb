272f1224c58b1ac37a935276853b4c1d7ed06c99
Daisy Magnus-Aryitey <daisymagnusaryitey@gmail.com>
2015-05-06 07:01:15 -0400

    Added second method and sarcastic comments.

--- gg.rb
+++ gg.rb
@@ -1,28 +1,38 @@
-
-
-gue = rand(1..101).to_i
-
 puts "Please guess a number between 1 and 100."
+guess = rand(1..100).to_i
 counter = 0
-array = []
 
 
-while counter < 5
-  number = gets.chomp.to_i
-  array << number
-  if number > guess
-    puts "Guess a lower number."
-    counter += 1
-  elsif number < guess
-    puts "Guess a higher number"
-    counter += 1
-  else number == guess
-    puts  "You got it! You win!"
-    break
-  end
-
-  if counter == 5
-    puts "You lose. The correct number was #{guess}"
-    puts array
+A = []
+def check(number)
+  if A.include?(number)
+    respond(number)
+  else
+    A << number
   end
 end
+
+Reply = ["C'mon! You just said that!", "You're pathetic at this.", "Seriously?!"]
+def respond(number)
+    if A.include?(number)
+      puts Reply[rand(3)]
+    end
+end
+
+while counter < 5
+      number = gets.chomp.to_i
+      check(number)
+    if number > guess
+        puts "Guess a lower number!"
+        counter += 1
+    elsif number < guess
+        puts "Guess a higher number!"
+        counter += 1
+    else number == guess
+        puts  "You got it! You win!"
+    end
+end
+
+  if counter == 5
+    puts "You lose. The correct number was #{guess}."
+  end
